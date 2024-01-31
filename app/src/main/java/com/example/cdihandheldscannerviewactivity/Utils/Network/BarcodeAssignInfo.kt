07ac9a91749b45d99a8cdbaed990b63e9c1c0d6d
Angel <adejesus@comdist.com>
2024-01-31 09:21:13 -0400

    Created BarcodeAssignInfo.kt for data classes

---
+++ app/src/main/java/com/example/cdihandheldscannerviewactivity/Utils/Network/BarcodeAssignInfo.kt
@@ -0,0 +1,7 @@
+package com.example.cdihandheldscannerviewactivity.Utils.Network
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+
+// All the JSON Response Data classes for the BarcodeAssign backend end point
