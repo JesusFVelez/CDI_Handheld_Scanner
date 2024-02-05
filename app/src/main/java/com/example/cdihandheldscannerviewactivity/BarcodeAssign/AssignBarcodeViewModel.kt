94a0be9356723d0f60d0ac39a7220ba939772bf4
Angel <adejesus@comdist.com>
2024-02-05 14:52:25 -0400

    Added all variables for every API call response and started working on API call functions.

--- app/src/main/java/com/example/cdihandheldscannerviewactivity/BarcodeAssign/AssignBarcodeViewModel.kt
+++ app/src/main/java/com/example/cdihandheldscannerviewactivity/BarcodeAssign/AssignBarcodeViewModel.kt
@@ -3,6 +3,8 @@ package com.example.cdihandheldscannerviewactivity.BarcodeAssign
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.cdihandheldscannerviewactivity.Utils.Network.GetItem
+import com.example.cdihandheldscannerviewactivity.Utils.Network.ResponseWasItemFound
 
 class AssignBarcodeViewModel: ViewModel() {
 
@@ -11,10 +13,51 @@ class AssignBarcodeViewModel: ViewModel() {
     val currentlyChosenAdapterPosition: LiveData<Int>
         get() = _currentlyChosenAdapterPosition
 
-    // Item number searched
-    private val _itemNumber = MutableLiveData<String>()
-    val itemNumber: LiveData<String>
-        get() = _itemNumber
+    //******************//
+    // API Calls Values //
+    //******************//
+
+    // LiveData for wasItemFound API call //
+
+    private val _wasItemFound = MutableLiveData<Boolean>()
+    val wasItemFound: LiveData<Boolean>
+        get() = _wasItemFound
+
+    // LiveData for getItem API call //
+
+    private val _itemInfo = MutableLiveData<List<GetItem>>()
+    val itemInfo: LiveData<List<GetItem>>
+        get() = _itemInfo
+
+    // LiveData for validateBarcode API call //
+
+    private val _isBarcodeValid = MutableLiveData<Boolean>()
+    val isBarcodeValid: MutableLiveData<Boolean>
+        get() = _isBarcodeValid
+
+    private val _isBarcodeValidError = MutableLiveData<String>()
+    val isBarcodeValidError: MutableLiveData<String>
+        get() = _isBarcodeValidError
+
+    // LiveData for setBarcode API call //
+
+    private val _isBarcodeSet = MutableLiveData<Boolean>()
+    val isBarcodeSet: MutableLiveData<Boolean>
+        get() = _isBarcodeSet
+
+    private val _isBarcodeSetError = MutableLiveData<String>()
+    val isBarcodeSetError: MutableLiveData<Boolean>
+        get() = _isBarcodeSet
+
+    //*********************//
+    // API Calls Functions //
+    //*********************//
+
+    fun validateItem(itemNumber: String){
+        validateItem(itemNumber)
+
+    }
+
 
     fun setAdapterPosition(position: Int) {
         if (position >= 0)
