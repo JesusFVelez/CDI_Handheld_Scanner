7142e1cab9f755c11f0c8e5db8bb656caf7e2b30
Jesus <jesus_velez1998@hotmail.com>
2024-03-25 16:46:12 -0400

    changed packlage name from "example" to "comdist" the name of the domain for cdi

---
+++ app/src/main/java/com/comdist/cdihandheldscannerviewactivity/Utils/Network/RPMAccessDataClass.kt
@@ -0,0 +1,31 @@
+package com.comdist.cdihandheldscannerviewactivity.Utils.Network
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+
+// checkIfUserHasAccessToFunctionality data classes
+@JsonClass(generateAdapter = true)
+data class RPMAccessResponseWrapper(
+    val response: RPMAccessResponse
+)
+
+@JsonClass(generateAdapter = true)
+data class RPMAccessResponse(
+    @Json(name = "doesUserHaveAccessToFunc") val doesUserHaveAccessToFunc: Boolean,
+    @Json(name = "errorMessage") val errorMessage: String
+)
+
+
+
+// verifyIfClientUsesRPM data classes
+@JsonClass(generateAdapter = true)
+data class doesUserHaveRPMResponseWrapper(
+    val response:doesUserHaveRPMResponse
+)
+
+@JsonClass(generateAdapter = true)
+data class doesUserHaveRPMResponse(
+    @Json(name = "doesClientUseRPM") val doesClientUseRPM: Boolean
+)
+
