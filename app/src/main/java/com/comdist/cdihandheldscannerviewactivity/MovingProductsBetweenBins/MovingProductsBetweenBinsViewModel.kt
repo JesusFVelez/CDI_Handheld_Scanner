cc38efa2f2965c19b4937654775e46abed54456f
Jesus <jesus_velez1998@hotmail.com>
2024-03-27 11:59:04 -0400

    Started working on making the API calls from the ViewModel

--- app/src/main/java/com/comdist/cdihandheldscannerviewactivity/MovingProductsBetweenBins/MovingProductsBetweenBinsViewModel.kt
+++ app/src/main/java/com/comdist/cdihandheldscannerviewactivity/MovingProductsBetweenBins/MovingProductsBetweenBinsViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.comdist.cdihandheldscannerviewactivity.Utils.Network.ScannerAPI
 import com.comdist.cdihandheldscannerviewactivity.Utils.Network.WarehouseInfo
+import com.comdist.cdihandheldscannerviewactivity.Utils.Network.binInfo
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 import java.lang.Exception
@@ -17,10 +18,45 @@ class MovingProductsBetweenBinsViewModel: ViewModel() {
     val wasLastAPICallSuccessful : LiveData<Boolean>
         get() = _wasLastAPICallSuccessful
 
+    // Error Message
+    private val _errorMessage = MutableLiveData<MutableMap<String,String>>()
+    val errorMessage : LiveData<MutableMap<String,String>>
+        get() = _errorMessage
+
+    private val _listOfBinsInWarehouse = MutableLiveData<List<binInfo>>()
+    val listOfBinsInWarehouse : LiveData<List<binInfo>>
+        get() = _listOfBinsInWarehouse
+
+
 
     init{
+        _errorMessage.value = mutableMapOf(
+            "confirmBin" to "",
+            "confirmItem" to "",
+            "isQuantityValid" to "",
+            "moveItemBetweenBins" to "" )
 
+    fun getAllBinsFromBackend(warehouseNumber: Int){
+        // Exception handler for API call
+        val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+            _wasLastAPICallSuccessful.value = false
+            Log.i("Get All Bins - Bin Movement (exceptionHandler) " , "Error -> ${exception.message}")
+        }
+
+        // API call to get the products in order
+        try {
+            viewModelScope.launch(exceptionHandler) {
+                val response = ScannerAPI.getMovingItemsBetweenBinsService().getAllBins(warehouseNumber)
+                _wasLastAPICallSuccessful.value = true
+                _listOfBinsInWarehouse.value = response.response.listOfBinsWrapper.listOfBins
+            }
+
+        }catch (e: Exception){
+            _wasLastAPICallSuccessful.value = false
+            Log.i("Get All Bins - Bin Movement (e) " , "Error -> ${e.message}")
+        }
     }
+
 
 
 
