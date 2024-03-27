f1b65b42690d98b769b66f1e7ea7ff80732086e5
Angel <adejesus@comdist.com>
2024-03-27 14:47:43 -0400

    Started working on receiving products layout.

--- app/src/androidTest/java/com/comdist/cdihandheldscannerviewactivity/ReceivingProductsIntoBin/ReceivingProductsIntoBinFragment.kt
+++ app/src/androidTest/java/com/comdist/cdihandheldscannerviewactivity/ReceivingProductsIntoBin/ReceivingProductsIntoBinFragment.kt
@@ -1,10 +1,12 @@
-package com.example.cdihandheldscannerviewactivity
+package com.comdist.cdihandheldscannerviewactivity.ReceivingProductsIntoBin
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.comdist.cdihandheldscannerviewactivity.R
+
 class ReceivingProductsIntoBinFragment : Fragment() {
 
 
