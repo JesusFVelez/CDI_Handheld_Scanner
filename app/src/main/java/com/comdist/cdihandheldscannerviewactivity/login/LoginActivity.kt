b1f2cd082579f95638ce42c2d575a4bb3da7be32
Jesus <jesus_velez1998@hotmail.com>
2024-04-04 15:41:49 -0400

    made one minor change to the login activity

--- app/src/main/java/com/comdist/cdihandheldscannerviewactivity/login/LoginActivity.kt
+++ app/src/main/java/com/comdist/cdihandheldscannerviewactivity/login/LoginActivity.kt
@@ -199,12 +199,12 @@ class LoginActivity : AppCompatActivity() {
     // Populate Spinner with warehouse data
     private fun fillSpinnerWithWarehouses( newWarehouseList : List<WarehouseInfo>){
         val warehouses = mutableListOf<String>()
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, warehouses)
-        warehouseSpinner.setAdapter(adapter)
+
         for (aWarehouse in viewModel.listOfWarehouses.value!!) {
             warehouses.add(aWarehouse.warehouseName)
         }
-        adapter.notifyDataSetChanged()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, warehouses)
+        warehouseSpinner.setAdapter(adapter)
     }
 
     // This method populates the Spinner with a list of companies. It creates an ArrayAdapter with the company names and sets it as the adapter for the Spinner.
