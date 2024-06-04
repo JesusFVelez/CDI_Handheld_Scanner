ca2a5f260f9411fca0daf8c41319a212158bab57
DiegoRiveraSantiago <drivera@comdist.com>
2024-06-04 08:33:02 -0400

    Started implementing button filters

--- app/src/main/java/com/scannerapp/cdihandheldscannerviewactivity/ProductPhysicalCount/ProductPhysicalCountViewModel.kt
+++ app/src/main/java/com/scannerapp/cdihandheldscannerviewactivity/ProductPhysicalCount/ProductPhysicalCountViewModel.kt
@@ -5,13 +5,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.comdist.cdihandheldscannerviewactivity.Utils.Network.DataClassesForAPICalls.TtBinInfo
+import com.comdist.cdihandheldscannerviewactivity.Utils.Network.DataClassesForAPICalls.BinInfo
 import com.comdist.cdihandheldscannerviewactivity.Utils.Network.DataClassesForAPICalls.TtItemInfo
 import com.scannerapp.cdihandheldscannerviewactivity.Utils.Network.ScannerAPI
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 
-class InventoryCountViewModel: ViewModel() {
+class InventoryCountViewModel : ViewModel() {
 
     private val _opSuccess = MutableLiveData<Boolean>()
     val opSuccess: LiveData<Boolean>
@@ -25,8 +25,8 @@ class InventoryCountViewModel: ViewModel() {
     val itemInfo: LiveData<List<TtItemInfo>>
         get() = _itemInfo
 
-    private val _binInfo = MutableLiveData<List<TtBinInfo>>()
-    val binInfo: LiveData<List<TtBinInfo>>
+    private val _binInfo = MutableLiveData<List<BinInfo>>()
+    val binInfo: LiveData<List<BinInfo>>
         get() = _binInfo
 
     private val _wasLastAPICallSuccessful = MutableLiveData<Boolean>()
@@ -34,26 +34,33 @@ class InventoryCountViewModel: ViewModel() {
         get() = _wasLastAPICallSuccessful
 
     private val _companyIDOfUser = MutableLiveData<String>()
-    val companyIDOfUser : LiveData<String>
+    val companyIDOfUser: LiveData<String>
         get() = _companyIDOfUser
 
     private val _warehouseNO = MutableLiveData<Int>()
     val warehouseNumberOfUser: LiveData<Int>
         get() = _warehouseNO
 
-    private val _currentlyChosenItemForSearch = MutableLiveData<TtBinInfo>()
-    val setCurrentlySelectedBin: LiveData<TtBinInfo>
+    private val _currentlyChosenItemForSearch = MutableLiveData<BinInfo>()
+    val setCurrentlySelectedBin: LiveData<BinInfo>
         get() = _currentlyChosenItemForSearch
 
+    // Added for saving filter states
+    val enteredItemNumber = MutableLiveData<String>()
+    val enteredVendor = MutableLiveData<String>()
+    val enteredClassCode = MutableLiveData<String>()
+    val selectedLane = MutableLiveData<String>("ALL")
+
     // Function to set the company ID from shared preferences
-    fun setCompanyIDFromSharedPref(companyID: String){
+    fun setCompanyIDFromSharedPref(companyID: String) {
         _companyIDOfUser.value = companyID
     }
 
-    fun setWarehouseNumberFromSharedPref(warehouseNumber: Int){
+    fun setWarehouseNumberFromSharedPref(warehouseNumber: Int) {
         _warehouseNO.value = warehouseNumber
     }
-    fun setCurrentlySelectedBin(selectedBin: TtBinInfo) {
+
+    fun setCurrentlySelectedBin(selectedBin: BinInfo) {
         _currentlyChosenItemForSearch.value = selectedBin
     }
 
@@ -66,7 +73,15 @@ class InventoryCountViewModel: ViewModel() {
         viewModelScope.launch(exceptionHandler) {
             try {
                 val response = ScannerAPI.getInventoryCountService().getAllBinNumbers(pCompanyID, pWarehouse)
-                _binInfo.value = response.response.ttBinInfo.ttBinInfo
+                Log.d("GetAllBinNumbers", "Response: ${response.response.ttBinInfo.ttBinInfo}")
+                _binInfo.value = response.response.ttBinInfo.ttBinInfo.map { bin ->
+                    BinInfo(
+                        binLocation = bin.binLocation,
+                        classCode = "",  // Set default values or get them from another source if available
+                        vendor = "",
+                        itemNumber = ""
+                    )
+                }
                 _wasLastAPICallSuccessful.value = true
             } catch (e: Exception) {
                 _wasLastAPICallSuccessful.value = false
@@ -116,9 +131,93 @@ class InventoryCountViewModel: ViewModel() {
         }
     }
 
+    fun getBinsByClassCode(pClassCode: String, pCompanyID: String, pWarehouseNo: Int) {
+        val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+            _wasLastAPICallSuccessful.value = false
+            Log.e("Get Bins By Class Code", "Failed to fetch bins: ${exception.localizedMessage}")
+        }
 
+        viewModelScope.launch(exceptionHandler) {
+            try {
+                val response = ScannerAPI.getInventoryCountService().getBinsByClassCode(pClassCode, pCompanyID, pWarehouseNo)
+                Log.d("GetBinsByClassCode", "Response: ${response.response.ttBinInfo.ttBinInfo}")
+
+                val bins = response.response.ttBinInfo.ttBinInfo.map { bin ->
+                    BinInfo(
+                        binLocation = bin.binLocation,
+                        classCode = bin.classCode ?: "N/A",
+                        vendor = bin.vendor ?: "N/A",
+                        itemNumber = bin.itemNumber
+                    )
+                }
+                Log.d("GetBinsByClassCode", "Parsed Bins: $bins")
+
+                _binInfo.value = bins
+                _wasLastAPICallSuccessful.value = true
+            } catch (e: Exception) {
+                _wasLastAPICallSuccessful.value = false
+                Log.e("Get Bins By Class Code", "Exception -> ${e.localizedMessage}")
+            }
+        }
+    }
+
+    fun getBinsByVendor(pVendor: String, pCompanyID: String, pWarehouseNo: Int) {
+        val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+            _wasLastAPICallSuccessful.value = false
+            Log.e("Get Bins By Vendor", "Failed to fetch bins: ${exception.localizedMessage}")
+        }
+
+        viewModelScope.launch(exceptionHandler) {
+            try {
+                val response = ScannerAPI.getInventoryCountService().getBinsByVendor(pVendor, pCompanyID, pWarehouseNo)
+                Log.d("GetBinsByVendor", "Response: ${response.response.ttBinInfo.ttBinInfo}")
+
+                val bins = response.response.ttBinInfo.ttBinInfo.map { bin ->
+                    BinInfo(
+                        binLocation = bin.binLocation,
+                        classCode = bin.classCode ?: "N/A",
+                        vendor = bin.vendor ?: "N/A",
+                        itemNumber = bin.itemNumber
+                    )
+                }
+                Log.d("GetBinsByVendor", "Parsed Bins: $bins")
+
+                _binInfo.value = bins
+                _wasLastAPICallSuccessful.value = true
+            } catch (e: Exception) {
+                _wasLastAPICallSuccessful.value = false
+                Log.e("Get Bins By Vendor", "Exception -> ${e.localizedMessage}")
+            }
+        }
+    }
+
+    fun getBinsByItemNumber(pItemNumber: String, pCompanyID: String, pWarehouseNo: Int) {
+        val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+            _wasLastAPICallSuccessful.value = false
+            Log.e("Get Bins By Item Number", "Failed to fetch bins: ${exception.localizedMessage}")
+        }
+
+        viewModelScope.launch(exceptionHandler) {
+            try {
+                val response = ScannerAPI.getInventoryCountService().getBinsByItemNumber(pItemNumber, pCompanyID, pWarehouseNo)
+                Log.d("GetBinsByItemNumber", "Response: ${response.response.ttBinInfo.ttBinInfo}")
+
+                val bins = response.response.ttBinInfo.ttBinInfo.map { bin ->
+                    BinInfo(
+                        binLocation = bin.binLocation,
+                        classCode = bin.classCode ?: "N/A",
+                        vendor = bin.vendor ?: "N/A",
+                        itemNumber = bin.itemNumber
+                    )
+                }
+                Log.d("GetBinsByItemNumber", "Parsed Bins: $bins")
+
+                _binInfo.value = bins
+                _wasLastAPICallSuccessful.value = true
+            } catch (e: Exception) {
+                _wasLastAPICallSuccessful.value = false
+                Log.e("Get Bins By Item Number", "Exception -> ${e.localizedMessage}")
+            }
+        }
+    }
 }
-
-
-
-
--- app/src/main/java/com/scannerapp/cdihandheldscannerviewactivity/ProductPhysicalCount/SearchBinProductPhysicalCountFragment.kt
+++ app/src/main/java/com/scannerapp/cdihandheldscannerviewactivity/ProductPhysicalCount/SearchBinProductPhysicalCountFragment.kt
@@ -1,21 +1,24 @@
 package com.scannerapp.cdihandheldscannerviewactivity.ProductPhysicalCount
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.comdist.cdihandheldscannerviewactivity.InventoryCount.InventoryCountViewModel
-import com.comdist.cdihandheldscannerviewactivity.Utils.Network.DataClassesForAPICalls.TtBinInfo
+import com.comdist.cdihandheldscannerviewactivity.Utils.Network.DataClassesForAPICalls.BinInfo
 import com.comdist.cdihandheldscannerviewactivity.adapters.BinItemAdapter
 import com.scannerapp.cdihandheldscannerviewactivity.R
 import com.scannerapp.cdihandheldscannerviewactivity.Utils.AlerterUtils
@@ -28,6 +31,7 @@ class SearchBinProductPhysicalCountFragment : Fragment() {
     private val viewModel: InventoryCountViewModel by activityViewModels()
     private lateinit var binItemAdapter: BinItemAdapter
     private lateinit var progressDialog: Dialog
+    private var allBinInfo: List<BinInfo> = listOf() // Initialize with empty list
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -38,11 +42,9 @@ class SearchBinProductPhysicalCountFragment : Fragment() {
         setupUI()
         initObservers()
 
-        // Retrieve company ID and warehouse number from shared preferences
         val companyID = SharedPreferencesUtils.getCompanyIDFromSharedPref(requireContext())
         val warehouseNumber = SharedPreferencesUtils.getWarehouseNumberFromSharedPref(requireContext())
 
-        // Set these values in the ViewModel
         viewModel.setCompanyIDFromSharedPref(companyID)
         viewModel.setWarehouseNumberFromSharedPref(warehouseNumber)
 
@@ -52,35 +54,109 @@ class SearchBinProductPhysicalCountFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         binding.binNumberSearchEditText.text.clear()
+        restoreFilterStates()
     }
 
     private fun setupUI() {
-        val warehouseNO = SharedPreferencesUtils.getWarehouseNumberFromSharedPref(requireContext())
-        viewModel.setWarehouseNumberFromSharedPref(warehouseNO)
-
-        val companyID = SharedPreferencesUtils.getCompanyIDFromSharedPref(requireContext())
-        viewModel.setCompanyIDFromSharedPref(companyID)
-
         progressDialog = PopupWindowUtils.getLoadingPopup(requireContext())
 
-        binItemAdapter = BinItemAdapter(requireContext()) { selectedBin ->
-            // Navigate to the next fragment when a bin is selected
+        binItemAdapter = BinItemAdapter(requireContext()) { selectedBin: BinInfo ->
             navigateToEditAndSearchItemFragment(selectedBin)
         }
         binding.binSearchList.adapter = binItemAdapter
 
         progressDialog.show()
-        viewModel.getAllBinNumbers(companyID, warehouseNO)
+        val companyID = SharedPreferencesUtils.getCompanyIDFromSharedPref(requireContext())
+        val warehouseNumber = SharedPreferencesUtils.getWarehouseNumberFromSharedPref(requireContext())
+        viewModel.getAllBinNumbers(companyID, warehouseNumber)
+
+        binding.laneFilterButton.setOnClickListener {
+            showLaneSelectionDialog()
+        }
+
+        binding.classCodeFilterButton.setOnClickListener {
+            val classCode = binding.binNumberSearchEditText.text.toString().trim()
+            if (viewModel.enteredClassCode.value?.isNotEmpty() == true && classCode.isEmpty()) {
+                viewModel.enteredClassCode.value = ""
+                binding.classCodeFilterButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.CDI_Gray)
+                viewModel.getAllBinNumbers(companyID, warehouseNumber)
+            } else {
+                viewModel.enteredClassCode.value = classCode
+                if (classCode.isNotEmpty()) {
+                    binding.binNumberSearchEditText.text.clear()
+                    progressDialog.show()
+                    viewModel.getBinsByClassCode(classCode, companyID, warehouseNumber)
+                    binding.classCodeFilterButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.CDI_Light_Blue)
+                }
+            }
+        }
+
+        binding.VendorFilterButton.setOnClickListener {
+            val vendor = binding.binNumberSearchEditText.text.toString().trim()
+            if (viewModel.enteredVendor.value?.isNotEmpty() == true && vendor.isEmpty()) {
+                viewModel.enteredVendor.value = ""
+                binding.VendorFilterButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.CDI_Gray)
+                viewModel.getAllBinNumbers(companyID, warehouseNumber)
+            } else {
+                viewModel.enteredVendor.value = vendor
+                if (vendor.isNotEmpty()) {
+                    binding.binNumberSearchEditText.text.clear()
+                    progressDialog.show()
+                    viewModel.getBinsByVendor(vendor, companyID, warehouseNumber)
+                    binding.VendorFilterButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.CDI_Light_Blue)
+                }
+            }
+        }
+
+        binding.ItemNumberFilterButton.setOnClickListener {
+            val itemNumber = binding.binNumberSearchEditText.text.toString().trim()
+            if (viewModel.enteredItemNumber.value?.isNotEmpty() == true && itemNumber.isEmpty()) {
+                viewModel.enteredItemNumber.value = ""
+                binding.ItemNumberFilterButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.CDI_Gray)
+                viewModel.getAllBinNumbers(companyID, warehouseNumber)
+            } else {
+                viewModel.enteredItemNumber.value = itemNumber
+                if (itemNumber.isNotEmpty()) {
+                    binding.binNumberSearchEditText.text.clear()
+                    progressDialog.show()
+                    viewModel.getBinsByItemNumber(itemNumber, companyID, warehouseNumber)
+                    binding.ItemNumberFilterButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.CDI_Light_Blue)
+                }
+            }
+        }
+
+        binding.binNumberSearchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val trimmedText = s.toString().trim()
+                if (trimmedText.isEmpty()) {
+                    applyFilters()
+                    binding.classCodeFilterButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.CDI_Gray) // Reset to default color
+                    binding.VendorFilterButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.CDI_Gray) // Reset to default color
+                    binding.ItemNumberFilterButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.CDI_Gray) // Reset to default color
+                } else {
+                    val filteredList = filteredBinInfoByLane().filter { binInfo: BinInfo ->
+                        binInfo.binLocation.contains(trimmedText, ignoreCase = true)
+                    }
+                    binItemAdapter.updateData(filteredList)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
     private fun initObservers() {
-        viewModel.binInfo.observe(viewLifecycleOwner) { newBinInfo ->
+        viewModel.binInfo.observe(viewLifecycleOwner) { newBinInfo: List<BinInfo> ->
             progressDialog.dismiss()
+            Log.d("SearchBinFragment", "New Bin Info: $newBinInfo")
+            allBinInfo = newBinInfo
+            applyFilters()
             initBinAutoCompleteTextView(newBinInfo)
-            binItemAdapter.updateData(newBinInfo) // Ensure initial data is loaded into the adapter
         }
 
-        viewModel.wasLastAPICallSuccessful.observe(viewLifecycleOwner) { wasLastAPICallSuccessful ->
+        viewModel.wasLastAPICallSuccessful.observe(viewLifecycleOwner) { wasLastAPICallSuccessful: Boolean ->
             if (!wasLastAPICallSuccessful) {
                 progressDialog.dismiss()
                 AlerterUtils.startNetworkErrorAlert(requireActivity())
@@ -88,13 +164,13 @@ class SearchBinProductPhysicalCountFragment : Fragment() {
         }
     }
 
-    private fun initBinAutoCompleteTextView(newBinSuggestion: List<TtBinInfo>) {
+    private fun initBinAutoCompleteTextView(newBinSuggestion: List<BinInfo>) {
         val autoCompleteAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, newBinSuggestion.map { it.binLocation })
         (binding.binNumberSearchEditText as? AutoCompleteTextView)?.apply {
             setAdapter(autoCompleteAdapter)
             threshold = 1
 
-            onItemClickListener = AdapterView.OnItemClickListener { parent, _, position, _ ->
+            onItemClickListener = AdapterView.OnItemClickListener { parent: AdapterView<*>, _, position: Int, _ ->
                 val selectedBinLocation = parent.getItemAtPosition(position) as String
                 val selectedBin = newBinSuggestion.first { it.binLocation == selectedBinLocation }
                 binItemAdapter.updateData(listOf(selectedBin))
@@ -108,10 +184,10 @@ class SearchBinProductPhysicalCountFragment : Fragment() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 val trimmedText = s.toString().trim()
                 if (trimmedText.isEmpty()) {
-                    binItemAdapter.updateData(newBinSuggestion)
+                    applyFilters()
                 } else {
-                    val filteredList = newBinSuggestion.filter {
-                        it.binLocation.contains(trimmedText, ignoreCase = true)
+                    val filteredList = filteredBinInfoByLane().filter { binInfo: BinInfo ->
+                        binInfo.binLocation.contains(trimmedText, ignoreCase = true)
                     }
                     binItemAdapter.updateData(filteredList)
                 }
@@ -121,9 +197,67 @@ class SearchBinProductPhysicalCountFragment : Fragment() {
         })
     }
 
-    private fun navigateToEditAndSearchItemFragment(selectedBin: TtBinInfo) {
-        viewModel.setCurrentlySelectedBin(selectedBin) // Assuming this method exists in your ViewModel
+    private fun navigateToEditAndSearchItemFragment(selectedBin: BinInfo) {
+        viewModel.setCurrentlySelectedBin(selectedBin)
         view?.findNavController()?.navigate(R.id.EditAndSearchItemProductPhysicalCountFragment)
     }
-}
 
+    private fun showLaneSelectionDialog() {
+        val lanes = allBinInfo.map { it.binLocation.take(2) }.distinct().sorted().toMutableList().apply { add(0, "ALL") }.toTypedArray()
+        AlertDialog.Builder(requireContext()).apply {
+            setTitle("Select Lane")
+            setItems(lanes) { _, which ->
+                viewModel.selectedLane.value = lanes[which]
+                applyFilters()
+            }
+            show()
+        }
+    }
+
+    private fun filteredBinInfoByLane(): List<BinInfo> {
+        return if (viewModel.selectedLane.value == "ALL") {
+            allBinInfo
+        } else {
+            allBinInfo.filter { it.binLocation.startsWith(viewModel.selectedLane.value ?: "ALL", ignoreCase = true) }
+        }
+    }
+
+    private fun applyFilters() {
+        Log.d("SearchBinFragment", "Applying Filters: selectedLane=${viewModel.selectedLane.value}, enteredClassCode=${viewModel.enteredClassCode.value}, enteredVendor=${viewModel.enteredVendor.value}, enteredItemNumber=${viewModel.enteredItemNumber.value}")
+
+        val filteredList = allBinInfo.filter { binInfo ->
+            val matchesLane = (viewModel.selectedLane.value == "ALL" || binInfo.binLocation.startsWith(viewModel.selectedLane.value ?: "ALL", ignoreCase = true))
+            matchesLane
+        }
+
+        Log.d("SearchBinFragment", "Filtered List: $filteredList")
+        binItemAdapter.updateData(filteredList)
+    }
+
+    private fun restoreFilterStates() {
+        binding.binNumberSearchEditText.setText(viewModel.enteredItemNumber.value)
+        viewModel.selectedLane.value?.let { selectedLane ->
+            if (selectedLane != "ALL") {
+                showLaneSelectionDialog()
+            }
+        }
+        viewModel.enteredClassCode.value?.let { classCode ->
+            if (classCode.isNotEmpty()) {
+                binding.binNumberSearchEditText.setText(classCode)
+                binding.classCodeFilterButton.performClick()
+            }
+        }
+        viewModel.enteredVendor.value?.let { vendor ->
+            if (vendor.isNotEmpty()) {
+                binding.binNumberSearchEditText.setText(vendor)
+                binding.VendorFilterButton.performClick()
+            }
+        }
+        viewModel.enteredItemNumber.value?.let { itemNumber ->
+            if (itemNumber.isNotEmpty()) {
+                binding.binNumberSearchEditText.setText(itemNumber)
+                binding.ItemNumberFilterButton.performClick()
+            }
+        }
+    }
+}
